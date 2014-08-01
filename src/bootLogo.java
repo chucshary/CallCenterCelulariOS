3c4862665721d825c735c2fc2efbd9ee50556bd2
Mariana V <marivale_0@hotmail.com>
2014-08-01 05:37:06 -0500

    lol

--- src/bootLogo.java
+++ src/bootLogo.java
@@ -1,4 +1,5 @@
 
+import java.awt.event.KeyEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
@@ -33,7 +34,7 @@ public class bootLogo extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        boton = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         celular = new javax.swing.JTextField();
         nombre = new javax.swing.JTextField();
@@ -45,13 +46,13 @@ public class bootLogo extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Login");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        boton.setText("Login");
+        boton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                botonActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 550, -1, -1));
+        jPanel1.add(boton, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 550, -1, -1));
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
@@ -98,7 +99,7 @@ public class bootLogo extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void botonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonActionPerformed
         // TODO add your handling code here:
         if (nombre.getText().length()!=0 &&nombre.getText().length()<31 && celular.getText().length()==10||nombre.getText().length()!=0 &&nombre.getText().length()<31 && celular.getText().length()==8)
         {
@@ -127,7 +128,7 @@ public class bootLogo extends javax.swing.JFrame {
             nombre.setText("");
             celular.setText("");
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_botonActionPerformed
 
     private void nombreKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nombreKeyTyped
         // TODO add your handling code here:
@@ -200,8 +201,8 @@ public class bootLogo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton boton;
     private javax.swing.JTextField celular;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
